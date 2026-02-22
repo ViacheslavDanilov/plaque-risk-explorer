285a9033632337c4b00fb13225c18d00fe79d662
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2026-02-22 15:20:23 +0100

    chore: import app module from plaque_risk_explorer

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,5 @@ CURRENT_DIR = os.path.dirname(__file__)
 SRC_DIR = os.path.join(CURRENT_DIR, "src")
 if SRC_DIR not in sys.path:
     sys.path.append(SRC_DIR)
+
+from plaque_risk_explorer.main import app  # noqa: F401, E402
